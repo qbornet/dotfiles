e2f5b472236d2bb2de9c3ced7e293e8ce4619588
Quentin BORNET <qbornet@e2r10p1.clusters.42paris.fr>
2023-06-22 07:44:00 +0200

    changed position of require so packer is init in first

--- other/nvim/lua/qbornet/init.lua
+++ other/nvim/lua/qbornet/init.lua
@@ -1,13 +1,12 @@
 local path = vim.fn.glob("~/.local/share/nvim/site/pack/packer/start/packer.nvim")
 
 if path == "" then
-    os.execute("git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim")
+    os.execute("git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim")
 end
 
-require("qbornet.remap")
-require("qbornet.set")
 require("qbornet.packer")
+require("qbornet.set")
+require("qbornet.remap")
 require("qbornet.visual")
 require("qbornet.ctags")
 require("qbornet.modified")
