d37070f4d12efaa0725ba587e5920eae2caae6c1
qbornet <ring0.csgo@gmail.com>
2023-11-16 01:19:02 +0100

    added tmux.conf

--- install_42.sh
+++ install_42.sh
@@ -6,10 +6,10 @@ fi
 
 cp ./other/zsh-theme/hyperzsh.zsh-theme $ZSH_CUSTOM/themes/
 
-if [ -z "$XDG_CONFIG_HOME" ]; then
-    mkdir -p ~/.config 2> /dev/null
-    cp -R ./other/nvim ~/.config/nvim
-else
-    mkdir -p $XDG_CONFIG_HOME 2> /dev/null
-    cp -R ./other/nvim $XDG_CONFIG_HOME/nvim
-fi
+#if [ -z "$XDG_CONFIG_HOME" ]; then
+#    mkdir -p ~/.config 2> /dev/null
+#    cp -R ./other/nvim ~/.config/nvim
+#else
+#    mkdir -p $XDG_CONFIG_HOME 2> /dev/null
+#    cp -R ./other/nvim $XDG_CONFIG_HOME/nvim
+#fi
