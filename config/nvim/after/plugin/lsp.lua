ef76fc8dcdac7633b157c327be215225fac9ca8f
qbornet <qbornet@proton.me>
2025-10-06 14:26:11 +0200

    [wip] testing.

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -26,6 +26,9 @@ local servers = {
 }
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsp_config = vim.lsp.config()
+
+print(lsp_config)
 
 
 -- lspconfig is required for clangd weird behavior with mason-lspconfig.
@@ -48,7 +51,7 @@ require('mason-lspconfig').setup({
     automatic_enable = true,
     handlers = {
         function(server_name)
-            vim.lsdp.config()[server_name].setup({
+            vim.lsp.config()[server_name].setup({
                 capabilities = lsp_capabilities,
             })
         end,
